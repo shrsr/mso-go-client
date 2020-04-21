f027a640e6b6f8b9fbad6e58a7fe72f8812373da
Payal Shah <payal.shah@CDSYS.LOCAL>
2020-04-21 15:28:19 +0530

    Created model file for schema

--- models/schema.go
+++ models/schema.go
@@ -1,27 +1,13 @@
 package models
 
-
-import (
-	"fmt"
-	"strconv"
-
-	"github.com/ciscoecosystem/mso-go-client/container"
-)
-
-
-
-
 type SchemaAttributes struct {
 	
 	
 	Schema string `json:",omitempty"`
 	
+	Templates []string `json:",omitempty"`
 	
-    
-	Templates   [] `json:",omitempty"`
-	
-    
-	Sites      [] `json:",omitempty"`
+    Sites      []string  `json:",omitempty"`
 	
     
 }
@@ -30,83 +16,64 @@ type SchemaAttributes struct {
 
 func NewSchemacontainer(schemaAttr SchemaAttributes) *SchemaAttributes {
 	
-        
-		SchemacontainerAttributes:= schemaAttr,
+	    SchemacontainerAttributes:= schemaAttr
 		return &SchemacontainerAttributes
-         
-	}
+}
 
    
-
-
-
-func (fvTenant *Tenant) ToMap() (map[string]interface{}, error) {
-	fvTenantMap, err := fvTenant.BaseAttributes.ToMap()
-	if err != nil {
-		return nil, err
-	}
-
+func (schemaAttributes *SchemaAttributes) ToMap() (map[string]interface{}, error) {
+schemaAttributeMap :=make(map[string]interface{})
+ A(schemaAttributeMap, "schema",schemaAttributes.Schema)
+A(schemaAttributeMap, "templates",schemaAttributes.Templates)
+A(schemaAttributeMap, "sites",schemaAttributes.Sites)
 	
-	
-	A(fvTenantMap, "name",fvTenant.Name)
-	
-	
-    
-	A(fvTenantMap, "annotation",fvTenant.Annotation)
-	
-    
-	A(fvTenantMap, "nameAlias",fvTenant.NameAlias)
-	
-    
-	
-
-	return fvTenantMap, err
+    return schemaAttributeMap, nil
 }
 
-func TenantFromContainerList(cont *container.Container, index int) *Tenant {
+// func TenantFromContainerList(cont *container.Container, index int) *Tenant {
 
-	TenantCont := cont.S("imdata").Index(index).S(FvtenantClassName, "attributes")
-	return &Tenant{
-		BaseAttributes{
-			DistinguishedName: G(TenantCont, "dn"),
-			Description:       G(TenantCont, "descr"),
-			Status:            G(TenantCont, "status"),
-			ClassName:         FvtenantClassName,
-			Rn:                G(TenantCont, "rn"),
-		},
+// 	TenantCont := cont.S("imdata").Index(index).S(FvtenantClassName, "attributes")
+// 	return &Tenant{
+// 		BaseAttributes{
+// 			DistinguishedName: G(TenantCont, "dn"),
+// 			Description:       G(TenantCont, "descr"),
+// 			Status:            G(TenantCont, "status"),
+// 			ClassName:         FvtenantClassName,
+// 			Rn:                G(TenantCont, "rn"),
+// 		},
         
-		TenantAttributes{
+// 		TenantAttributes{
 		
 		
-			Name : G(TenantCont, "name"),
+// 			Name : G(TenantCont, "name"),
 		
-		
-        
-	        Annotation : G(TenantCont, "annotation"),
 		
         
-	        NameAlias : G(TenantCont, "nameAlias"),
+// 	        Annotation : G(TenantCont, "annotation"),
+		
+        
+// 	        NameAlias : G(TenantCont, "nameAlias"),
 		
         		
-        },
+//         },
         
-	}
-}
+// 	}
+// }
 
-func TenantFromContainer(cont *container.Container) *Tenant {
+// func TenantFromContainer(cont *container.Container) *Tenant {
 
-	return TenantFromContainerList(cont, 0)
-}
+// 	return TenantFromContainerList(cont, 0)
+// }
 
-func TenantListFromContainer(cont *container.Container) []*Tenant {
-	length, _ := strconv.Atoi(G(cont, "totalCount"))
+// func TenantListFromContainer(cont *container.Container) []*Tenant {
+// 	length, _ := strconv.Atoi(G(cont, "totalCount"))
 
-	arr := make([]*Tenant, length)
+// 	arr := make([]*Tenant, length)
 
-	for i := 0; i < length; i++ {
+// 	for i := 0; i < length; i++ {
 
-		arr[i] = TenantFromContainerList(cont, i)
-	}
+// 		arr[i] = TenantFromContainerList(cont, i)
+// 	}
 
-	return arr
-}
+// 	return arr
+// }
