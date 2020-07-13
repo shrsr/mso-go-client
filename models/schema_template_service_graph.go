e5c623eaab15567a0f968567e35b22699dcf67ce
nkatarmal-crest <nirav.katarmal@crestdatasys.com>
2020-07-13 15:28:34 +0530

    added upload generic payload

--- models/schema_template_service_graph.go
+++ models/schema_template_service_graph.go
@@ -6,6 +6,19 @@ type TemplateServiceGraph struct {
 	Value map[string]interface{}
 }
 
+type TemplateServiceGraphUpdate struct {
+	Ops   string `json:",omitempty"`
+	Path  string `json:",omitempty"`
+	Value interface{}
+}
+
+func NewTemplateServiceGraphUpdate(ops, path string, graphRef interface{}) *TemplateServiceGraphUpdate {
+	return &TemplateServiceGraphUpdate{
+		Ops:   ops,
+		Path:  path,
+		Value: graphRef,
+	}
+}
 func NewTemplateServiceGraph(ops, path string, graphRef map[string]interface{}) *TemplateServiceGraph {
 
 	return &TemplateServiceGraph{
@@ -14,6 +27,17 @@ func NewTemplateServiceGraph(ops, path string, graphRef map[string]interface{}) 
 		Value: graphRef,
 	}
 
+}
+
+func (graphAttributes *TemplateServiceGraphUpdate) ToMap() (map[string]interface{}, error) {
+	graphAttributesMap := make(map[string]interface{})
+	A(graphAttributesMap, "op", graphAttributes.Ops)
+	A(graphAttributesMap, "path", graphAttributes.Path)
+	if graphAttributes.Value != nil {
+		A(graphAttributesMap, "value", graphAttributes.Value)
+	}
+
+	return graphAttributesMap, nil
 }
 
 func (graphAttributes *TemplateServiceGraph) ToMap() (map[string]interface{}, error) {
