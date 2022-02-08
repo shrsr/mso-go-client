8816623c124ec46c30b8d60860fa71f1b7413a6a
shrsr <ssrish@cisco.com>
2022-02-07 16:28:34 -0800

    add new parameters

--- models/templateBD.go
+++ models/templateBD.go
@@ -6,19 +6,24 @@ type TemplateBD struct {
 	Value map[string]interface{} `json:",omitempty"`
 }
 
-func NewTemplateBD(ops, path, name, displayName, layer2Unicast string, intersiteBumTrafficAllow, optimizeWanBandwidth, l2Stretch, l3MCast bool, vrfRef, dhcpLabel map[string]interface{}) *TemplateBD {
+func NewTemplateBD(ops, path, name, displayName, layer2Unicast, unknownMulticastFlooding, multiDestinationFlooding, ipv6UnknownMulticastFlooding, virtualMacAddress string, intersiteBumTrafficAllow, optimizeWanBandwidth, l2Stretch, l3MCast, arpFlooding, unicastRouting bool, vrfRef, dhcpLabel map[string]interface{}) *TemplateBD {
 	var bdMap map[string]interface{}
 	bdMap = map[string]interface{}{
-		"name":                     name,
-		"displayName":              displayName,
-		"l2UnknownUnicast":         layer2Unicast,
-		"intersiteBumTrafficAllow": intersiteBumTrafficAllow,
-		"optimizeWanBandwidth":     optimizeWanBandwidth,
-		"l2Stretch":                l2Stretch,
-		"l3MCast":                  l3MCast,
-		"vrfRef":                   vrfRef,
-		"dhcpLabel":                dhcpLabel,
-		"subnets":                  []interface{}{},
+		"name":                         name,
+		"displayName":                  displayName,
+		"l2UnknownUnicast":             layer2Unicast,
+		"unknownMulticastFlooding":     unknownMulticastFlooding,
+		"multiDestinationFlooding":     multiDestinationFlooding,
+		"ipv6UnknownMulticastFlooding": ipv6UnknownMulticastFlooding,
+		"virtualMacAddress":            virtualMacAddress,
+		"arpFlooding":                  arpFlooding,
+		"unicastRouting":               unicastRouting,
+		"intersiteBumTrafficAllow":     intersiteBumTrafficAllow,
+		"optimizeWanBandwidth":         optimizeWanBandwidth,
+		"l2Stretch":                    l2Stretch,
+		"l3MCast":                      l3MCast,
+		"vrfRef":                       vrfRef,
+		"dhcpLabel":                    dhcpLabel,
 	}
 
 	if bdMap["l2UnknownUnicast"] == "" {
