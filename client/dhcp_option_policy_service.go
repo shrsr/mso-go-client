4f124dd74081900f4b2489b78c531800a2cc63b7
RutvikS-crest <rutvik.shiyani@crestdatasys.com>
2022-02-09 19:05:11 +0530

    added model and service files for dhcp_option_policy

--- client/dhcp_option_policy_service.go
+++ client/dhcp_option_policy_service.go
@@ -1,9 +1,27 @@
 package client
 
 import (
+	"fmt"
+
 	"github.com/ciscoecosystem/mso-go-client/container"
 	"github.com/ciscoecosystem/mso-go-client/models"
 )
+
+func (client *Client) GetDHCPOptionPolicyID(name string) (string, error) {
+	path := "api/v1/policies/dhcp/option"
+	cont, err := client.GetViaURL(path)
+	if err != nil {
+		return "", err
+	}
+	for _, policy := range cont.S("DhcpRelayPolicies").Data().([]interface{}) {
+		if optionPol, ok := policy.(map[string]interface{}); ok {
+			if name == optionPol["name"].(string) {
+				return optionPol["id"].(string), nil
+			}
+		}
+	}
+	return "", fmt.Errorf("DHCP Option Policy with name: %s not found", name)
+}
 
 func (client *Client) CreateDHCPOptionPolicy(obj *models.DHCPOptionPolicy) (*container.Container, error) {
 	path := "api/v1/policies/dhcp/option"
